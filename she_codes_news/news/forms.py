49dc8c01a80ba6a1b3f38af3ff746965959d38cf
Kriti Bhardwaj <kritibhardwaj@icloud.com>
2020-08-30 17:06:52 +1000

    Changes to set up basic News project

--- she_codes_news/news/forms.py
+++ she_codes_news/news/forms.py
@@ -5,5 +5,8 @@ from .models import NewsStory
 class StoryForm(ModelForm):
     class Meta:
         model = NewsStory
-        fields = ['title', 'author', 'pub_date', 'content']
-
+        fields = ['title', 'pub_date', 'content']
+        widgets ={
+            'pub_date': forms.DateInput(format=('%m/%d/%Y'),
+            attrs={'class':'form-control', 'placeholder':'Select a data','type':'date'})
+        }
---
+++ she_codes_news/users/models.py
@@ -0,0 +1,13 @@
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+
+# Create your models here.
+
+class CustomUser(AbstractUser):
+    #this is where I would put custom fields
+    pass
+
+    def __str__(self):
+        return self.username
+
+
